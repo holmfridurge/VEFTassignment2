c00e5c52b51a36bcb25fda37dcd6b4df804309e8
Þorvarður Bergmann <orvarur Bergmann>
2016-09-07 21:45:49 +0000

    Courses.cs updated

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -7,14 +7,14 @@ namespace WebApplication.Models
     public class Course
     {
         [Required]
-        public string Name { get; set; }
-        // example: "Web Services"
-        [Required]
         public string TemplateID { get; set; }
         // example: "T-514-VEFT"
         [Required]
         public int ID { get; set; }
         // example: 1
+        [Required]
+        public string Semester { get; set; }
+        // example: 20163
         [Required]
         public DateTime StartDate { get; set; }
         // example: "2016-08-17"
